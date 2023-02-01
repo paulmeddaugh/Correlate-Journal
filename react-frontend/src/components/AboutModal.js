5567557505c9116d1ca1f01832a8c8b5d92ab6d1
Paul Meddaugh <paul.meddaugh@gmail.com>
2023-02-01 17:35:07 -0600

    Made 'Click to view connections' label transition from the center of the side of a note rather than fixed to the top
    Revised README and AboutModel content
    
    Lowercased name of website
    
    Added a 192px image of favicon for phones

--- react-frontend/src/components/AboutModal.js
+++ react-frontend/src/components/AboutModal.js
@@ -17,13 +17,12 @@ function AboutModal(props) {
 			</Modal.Header>
 			<Modal.Body>
 				<p className={styles.description}>
-					This site is an online journal for following trains of thought throughout dated memoirs,
-					providing notes further ability to connect to any others. A notebook 
+					This site is an online journal for following trains of thought throughout date-ordered memoirs,
+					providing pages further ability to tie string to any other. A notebook 
 					and pencil are an amazing way to process or record thoughts, but sometimes a re-considering 
 					or snow-balling idea, scattered throughout date-ordered thoughts, can be difficult to follow 
-					amidst non-related ideas. Therefore, this site provides an 
-					additional web-layout for notes, on top of chronological order, for following trains of thought 
-					throughout a journal.
+					amidst non-related ideas. Therefore, this site provides an additional web-layout for notes, 
+					on top of chronological order, for following trains of thought throughout a journal.
 				</p>
 			</Modal.Body>
 			<Modal.Footer>
