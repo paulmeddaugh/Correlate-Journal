e97a5f07a1d7fd68c9f9ee9f31fd075fd5625651
Paul Meddaugh <paul.meddaugh@gmail.com>
2023-02-20 21:25:23 -0600

    Attempt to prevent preview of NoteBoxNote <a> href in Safari for easier reordering

--- react-frontend/src/components/NoteBox/NoteBoxNote.js
+++ react-frontend/src/components/NoteBox/NoteBoxNote.js
@@ -1,12 +1,6 @@
 import { forwardRef } from 'react';
 import styles from '../../styles/NoteBox/NoteBoxNote.module.css';
 
-/** Keeps track of the time in milliseconds when mouse is pressed down */
-let mouseDownStart = 0;
-
-/** The number of milliseconds for the mouse to be held down before calling onSelectDrag. */
-const ON_SELECT_DRAG_MILLI = 500;
-
 const NoteBoxNote = ({ note, index, onSelect, onSelectDrag, onSelectDrop, onDelete, selected, dragging, style }, ref) => {
 
     const deleteNote = (e) => {
@@ -16,36 +10,6 @@ const NoteBoxNote = ({ note, index, onSelect, onSelectDrag, onSelectDrop, onDele
     const selectNote = (e) => {
         onSelect?.(e, note, index);
     }
-
-    /**
-     * Determines if mouse is held down long enough to call onSelectDrag.
-     */
-    // const onMouseDown = (e) => {
-    //     mouseDownStart = new Date().getTime();
-
-    //     setTimeout(() => {
-    //         if (mouseDownStart) onSelectDrag?.(e, note, index);
-    //     }, ON_SELECT_DRAG_MILLI);
-    // }
-
-    /**
-     * Determines if mouse was held down long enough to call onSelectDrop.
-     */
-    // const onMouseUp = () => {
-    //     if (mouseDownStart + ON_SELECT_DRAG_MILLI < new Date().getTime()) {
-    //         onSelectDrop?.(note, index);
-    //     }
-    //     mouseDownStart = 0;
-    // }
-
-    /**
-     * Determines if the note is being dragged, and calls onSelectDrag if so.
-     */
-    // const onMouseMove = (e) => {
-    //     if (mouseDownStart && mouseDownStart + ON_SELECT_DRAG_MILLI < new Date().getTime()) {
-    //         onSelectDrag?.(e, note, index);
-    //     }
-    // }
 
     return (
         <a 
@@ -59,7 +23,8 @@ const NoteBoxNote = ({ note, index, onSelect, onSelectDrag, onSelectDrop, onDele
             href={'#' + note?.title ?? ''}
             style={Object.assign(style ?? {}, { order: note?.allNotesPosition })}
             className={"list-group-item list-group-item-action flex-column align-items-start " 
-                + ((selected.note?.id === note?.id && !dragging) ? "active " : ' ') // Is selected styling
+                + styles.container +
+                + ((selected.note?.id === note?.id && !dragging) ? "active " : ' ') // Selected styling
                 + (String(note?.title)[0] === '﻿' ? styles.unsavedNote + ' ' : ' ') // Unsaved note styling
                 + (dragging ? styles.reordering : '')}
             data-idnotebook={note?.idNotebook}
