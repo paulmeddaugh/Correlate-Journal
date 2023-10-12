ffc4d9d2dcfd27d25b044a97cc7feb1c120df68e
Paul Meddaugh <paul.meddaugh@gmail.com>
2023-10-12 11:39:24 -0500

    added railway config file to also retrieve frontend submodule
    additionally added some code to possibly fix spring server reloading issue when deploying

--- src/main/java/backend/redirect/RedirectOnResourceNotFoundException.java
+++ src/main/java/backend/redirect/RedirectOnResourceNotFoundException.java
@@ -4,23 +4,18 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@ControllerAdvice
-public class RedirectOnResourceNotFoundException  {
-	@ExceptionHandler(value = NoHandlerFoundException.class)
-    public Object handleStaticResourceNotFound(final NoHandlerFoundException ex, HttpServletRequest req, RedirectAttributes redirectAttributes) {
-        if (req.getRequestURI().startsWith("/api"))
-            return this.getApiResourceNotFoundBody(ex, req);
-        else {
-            redirectAttributes.addFlashAttribute("errorMessage", "My Custom error message");
-            return "forward:/index.html";
-        }
+@Controller
+public class RedirectOnResourceNotFoundException {
+
+    @RequestMapping(value = { "/", "/{x:[\\w\\-]+}", "/{x:^(?!api$).*$}/*/{y:[\\w\\-]+}","/error"  })
+    public String getIndex(HttpServletRequest request) {
+        return "/index.html";
     }
 
-    private ResponseEntity<String> getApiResourceNotFoundBody(NoHandlerFoundException ex, HttpServletRequest req) {
-        return new ResponseEntity<>("Not Found !!", HttpStatus.NOT_FOUND);
-    }
 }
